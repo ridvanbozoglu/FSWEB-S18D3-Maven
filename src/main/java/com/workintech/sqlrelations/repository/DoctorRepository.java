d59c3e735b0bb36d2fcf1c9745a8d6075eaaf0ef
ridvanbozoglu <ridvanbozoglu.5@gmail.com>
2024-10-25 09:00:44 +0300

    Task is done.

--- src/main/java/com/workintech/sqlrelations/repository/DoctorRepository.java
+++ src/main/java/com/workintech/sqlrelations/repository/DoctorRepository.java
@@ -2,6 +2,7 @@ package com.workintech.sqlrelations.repository;
 
 import com.workintech.sqlrelations.entity.Doctor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
 }
